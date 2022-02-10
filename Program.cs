9da0e3d71cef953751d723a6fb3addf618061734
josuedilmo <josuedilmo@gmail.com>
2022-02-10 15:01:05 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using ExercisesOOP_S2W4.Question1;
+using System;
+
+namespace ExercisesOOP_S2W4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercises S2W4");
+            Console.WriteLine("Question .1");
+            Console.WriteLine("Question .2");
+            Console.WriteLine("Question .3");
+            Console.Write("Choose a question: ");
+            int menuOpt = Convert.ToInt32(Console.ReadLine());
+            switch (menuOpt)
+            {
+                case 1:
+                    DeliverySystem system = new();
+                    system.deliverySystem();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
+            
+        }
+    }
+}
---
+++ Question1/DeliverySystem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercisesOOP_S2W4.Question1
+{
+    public class DeliverySystem
+    {
+        public void deliverySystem()
+        {
+            Console.WriteLine("Delivery System App");
+            Console.Write("Please, insert your zip code (DXX): ");
+            string userZipcode = Console.ReadLine();
+            string[] deliveryZipCode = { "D01", "D02", "D03", "D04", "D05", "D06", "D07", "D08", "D09", "D10" };
+            foreach(string zipCode in deliveryZipCode)
+            {
+                if (zipCode.Contains(userZipcode))
+                {
+                    Console.WriteLine("Zip code found.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Zip code not found.");
+                    break;
+                }
+            }
+        }
+    }
+}
